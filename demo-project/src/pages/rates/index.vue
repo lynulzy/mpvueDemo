5a0b5b60295b021f58215bd7e1df0cb78f6e3a9a
lizhiyuan <lizhiyuan@hupu.com>
2018-06-08 18:25:25 +0800

    list

--- demo-project/src/pages/rates/index.vue
+++ demo-project/src/pages/rates/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <rates-header></rates-header>
+    <rates-header class="header"></rates-header>
+    <ul class="list" v-for="item in items">
+      <li class="cell">
+        <div>
+          <p class="cellTitle">{{item.title}}</p>
+          <img class="cellIcon"></img>
+          <p class="cellRates">{{item.rates}}</p>
+          <div class="cellUnitName">{{item.unit}}</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +19,21 @@ import ratesHeader from "@/components/ratesHeader"
 export default {
   data () {
     return {
-      
+      items: [{title: '中国', rates: '4.5', unit: '港币1'}, 
+      {title: 'HK', rates: '4.5', unit: '美元'}, 
+      {title: 'TW', rates: '4.5', unit: '人民币'}, 
+      {title: 'US', rates: '4.5', unit: '新台币'}, 
+      {title: 'JPN', rates: '4.5', unit: '日元'}, 
+      {title: '中国', rates: '4.5', unit: '韩元'}, 
+      {title: 'HK', rates: '4.5', unit: '港币'}, 
+      {title: 'TW', rates: '4.5', unit: '韩元'}, 
+      {title: 'US', rates: '4.5', unit: '港币'}, 
+      {title: 'JPN', rates: '4.5', unit: '港币'},
+      {title: '中国', rates: '4.5', unit: '港币'}, 
+      {title: 'HK', rates: '4.5', unit: '港币'}, 
+      {title: 'TW', rates: '4.5', unit: '港币'}, 
+      {title: 'US', rates: '4.5', unit: '新台币'}, 
+      {title: 'JPN', rates: '4.5', unit: '港币1'}]
     }
   },
  components: {
@@ -19,5 +43,42 @@ export default {
 </script>
 
 <style lang="less">
+.header {
+}
+.list {
+  margin-top: 100px;
+}
+@cellHeight: 40px;
+.cell {
+  margin: 0 auto;
+  display: flex;
+  height: @cellHeight;
+  width: 100%;
+}
+.cellTitle {
+  margin-left: 30px;
+  background-color: antiquewhite;
+  width: 50%;
+}
+@iconHeight: 25px;
+.cellIcon {
+  margin-top: @cellHeight/2;
+  background-color: yellowgreen;
+  width: @iconHeight;
+  height: @iconHeight;
+  border-radius: @iconHeight/2;
+} 
+
+.cellRates {
+  margin-left: 100px;
+  background-color: azure;
+  width: 100%;
+}
+.cellUnitName {
+  background-color: burlywood;
+  margin-left: 150px;
+  margin-top: -30px;
+  width: 100%;
+}
 </style>
 
